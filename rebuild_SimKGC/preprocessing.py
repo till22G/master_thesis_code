d9fd5c6c844d4d075b92e004d4169880eee11615
till22G <tillman.galla@online.de>
2023-09-14 13:20:02 +0200

    added some code to also load and preprocess descriptions. Some descriptions do not exist. Build in light error handling code, for that case, but needs to be revised

--- rebuild_SimKGC/preprocessing.py
+++ rebuild_SimKGC/preprocessing.py
@@ -1,5 +1,6 @@
 # load data
 import json
+import numpy as np
 
 
 def _load_fbk15_237(path):
@@ -10,15 +11,16 @@ def _load_fbk15_237(path):
             lines = file.readlines()
             for line in lines:
                 triples.append(line.strip().split('\t'))
-    except:
-        print("FBK15_237 data could not be loaded")
                 
-    print("Data successfully loaded")                
-
+        print("FBK15_237 data was loaded successfully")
+        
+    except:
+        print("FBK15_237 data could not be loaded successfully")
+                              
     return triples
         
-        
-def _load_fbk15_237_names(path):
+
+def _load_fbk15_237_mid2names(path):
     entity_names = {}
     
     try:
@@ -28,15 +30,33 @@ def _load_fbk15_237_names(path):
                 code, name = line.strip().split('\t')
                 name = name.replace("_", " ")
                 entity_names[code] = name
-    except:
-        print("FBK15_237 names could not be loaded")
-              
-    print("Names successfully loaded")
+        
+        print("FBK15_237 names were loaded successfully")
+        
+    except:      
+        print("FBK15_237 names could not be loaded successfully")
+        
     return entity_names
+
+def _load_fbk15_237_mid2descriptions(path):
+    entity_descriptions = {}
+    
+    try:
+        with open (path, "r", encoding="utf-8") as file:
+            lines = file.readlines()
+            for line in lines:
+                code, description = line.strip().split('\t')
+                description = description.replace("\\", "")
+                entity_descriptions[code] = description
+                
+    except:
+        print("FBK15_237_mid2descriptions could not be loaded successfully")
+
+    return entity_descriptions
 
 
 def fbk15_237_to_json(triples, entity_names):
-    train_entries = []
+    entries = []
     
     for triple in triples:
         entry = {}
@@ -46,28 +66,62 @@ def fbk15_237_to_json(triples, entity_names):
         entry["tail_id"] = entity_names[triple[2]]
         entry["tail"] = triple[2]
 
-        train_entries.append(entry)
+        entries.append(entry)
         
+    filename = "myfile.json"
     try:
         print("Saving file ...") 
-        with open("myfile.json", "w") as out_file:
-            json.dump(train_entries, out_file, indent = 6)
+        with open(filename, "w") as out_file:
+            json.dump(entries, out_file, indent = 6)
+            
+        print("Data safed to {}".format(filename))
             
     except:
-        print("data could not be safed as .json") 
+        print("Data could not be saved as .json")
+        
+        
+def _save_FBK15_237_entities_to_json(entity_names, entity_descriptions):
+    entries = []
+    count = 0
+    
+    for key in entity_names:
+        entry = {}
+        entry["entity_id"] = key
+        entry["entity"] = entity_names[key]
+        if key in entity_descriptions:
+            entry["entity_desc"] = entity_descriptions[key]
+        else:
+            count += 1
+            print("Keys not found: {}".format(count))
+        
+        entries.append(entry)
+        
+    filename = "eneties.json"
+    try:
+        print("Saving file ...") 
+        with open(filename, "w") as out_file:
+            json.dump(entries, out_file, indent = 6)
             
+        print("Data saved to {}".format(filename))
+            
+    except:
+        pass
+    
             
 def main():
     
     print("Start pre-processing")
 
     triples = _load_fbk15_237("data/FB15K-237/train.txt")
-    entity_names = _load_fbk15_237_names("data/FB15K-237/FB15K_mid2name.txt")
+    entity_names = _load_fbk15_237_mid2names("data/FB15K-237/FB15K_mid2name.txt")
     fbk15_237_to_json(triples, entity_names)
+    
+    entity_descriptions = _load_fbk15_237_mid2descriptions("data/FB15K-237/FB15k_mid2description.txt")
+    _save_FBK15_237_entities_to_json(entity_names, entity_descriptions)
+        
+    print("Finished preprocessing")
 
 
 if __name__ == "__main__":
     main()
 
-    
-print("Script done")
