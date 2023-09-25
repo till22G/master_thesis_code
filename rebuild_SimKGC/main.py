58396201678562818a3a52bc3fade5ad967ea94a
till22G <tillman.galla@online.de>
2023-09-25 13:40:42 +0200

    renamed model.py to main.py. Also added a new model file, that will contain the custom bert model for SimKGC

---
+++ rebuild_SimKGC/main.py
@@ -0,0 +1,24 @@
+# this file should contain the model of SimKGC recreated from the description in the paper
+import argparse
+import torch
+
+from transformers import AutoModel, AutoTokenizer
+
+# load the bert-base-uncased model
+model_type = "bert-base-uncased"
+tokenizer  = AutoTokenizer.from_pretrained(model_type)
+bert_hr = AutoModel.from_pretrained(model_type) # create bert model for relation aware embeddings
+bert_t = AutoModel.from_pretrained(model_type) # create bert model for tail entity embeddings
+
+input_text = "I love this library!"
+inputs = tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
+
+with torch.no_grad():
+    outputs = bert_hr(**inputs)
+
+hidden_states = outputs.last_hidden_state
+
+print(hidden_states)
+print(hidden_states.size())
+#last_layer = model.base_model.encoder.layer[-1]
+
