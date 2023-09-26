35ddfca5267c82e24fc7d5aaeefa9821e8a1631e
till22G <tillman.galla@online.de>
2023-09-26 17:34:30 +0200

    added basic custom model

---
+++ rebuild_SimKGC/model.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+
+class CustomModel(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        
+    def forward():
+        pass 
+    
