c4c59d72a8a7b8681d96a770f31271e79661adff
till22G <tillman.galla@online.de>
2023-09-28 15:44:13 +0200

    added first code to model.py

--- rebuild_SimKGC/model.py
+++ rebuild_SimKGC/model.py
@@ -1,9 +1,20 @@
 import torch.nn as nn
+from transformers import AutoModel, AutoTokenizer
+
+def build_model(args) -> nn.Module:
+    return CustomModel(args)
 
 class CustomModel(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
+    def __init__(self, args) -> None:
+        super().__init__(args)
         
+        self.args = args
+        
+        model_type = "bert-base-uncased"
+        tokenizer  = AutoTokenizer.from_pretrained(model_type)
+        bert_hr = AutoModel.from_pretrained(model_type) # create bert model for relation aware embeddings
+        bert_t = AutoModel.from_pretrained(model_type) # create bert model for tail entity embeddings
+    
     def forward():
         pass 
     
