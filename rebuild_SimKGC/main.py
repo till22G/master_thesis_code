f8e6175060e931d841c46cb0c35ba0fcaf08a71f
till22G <tillman.galla@online.de>
2023-09-28 15:43:46 +0200

    cleaned up main.py

--- rebuild_SimKGC/main.py
+++ rebuild_SimKGC/main.py
@@ -6,26 +6,8 @@ from transformers import AutoModel, AutoTokenizer
 
 from logger import logger
 
-"""  load the bert-base-uncased model
-model_type = "bert-base-uncased"
-tokenizer  = AutoTokenizer.from_pretrained(model_type)
-bert_hr = AutoModel.from_pretrained(model_type) # create bert model for relation aware embeddings
-bert_t = AutoModel.from_pretrained(model_type) # create bert model for tail entity embeddings
-
-input_text = "I love this library!"
-inputs = tokenizer(input_text, return_tensors="pt", padding=True, truncation=True)
-
-with torch.no_grad():
-    outputs = bert_hr(**inputs)
-
-hidden_states = outputs.last_hidden_state
-
-print(hidden_states)
-print(hidden_states.size())
-#last_layer = model.base_model.encoder.layer[-1] """
-
 def main():
-    logger.info("Test info")
+    logger.info("Entered Main")
 
 if __name__ == "__main__":
     main()
