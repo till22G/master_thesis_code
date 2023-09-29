4e68d5f6160b78a540e872bb9f96e986fc378152
till22G <tillman.galla@online.de>
2023-09-29 14:41:04 +0200

    renamed parse

--- rebuild_SimKGC/parser_setup.py
+++
@@ -1,9 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(prog="replicated SimKGC",
-                                 description="Argparser for replicating SimKGC")
-
-parser.add_argument("--pretrained-model", default="bert-base-uncased", 
-                    type=str, help="define the tranformer model that is used for encoder initialization")
-parser.add_argument("--test-mode", action="store_true",
-                    help="Start SimKGC in test mode or not")
