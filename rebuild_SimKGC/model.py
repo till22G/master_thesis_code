b6ec4a4d6fc59ec3a0bcc369daf8f13c17ba2764
till22G <tillman.galla@online.de>
2023-10-06 17:26:34 +0200

    added mean pooling and normalization as described in the paper

--- rebuild_SimKGC/model.py
+++ rebuild_SimKGC/model.py
@@ -1,4 +1,5 @@
 import torch.nn as nn
+import torch
 from transformers import AutoModel, AutoTokenizer
 
 def build_model(args) -> nn.Module:
@@ -18,12 +19,17 @@ class CustomModel(nn.Module):
                        attention_mask=input_mask,
                        token_type_ids=input_token_type_ids,
                        return_dict=True)
-        last_hidden_state = output.last_hidden_state
-        print(last_hidden_state.size())
-        cls_output = last_hidden_state[:, 0, :]
-        print(cls_output)
         
-        return ""
+        last_hidden_states = output.last_hidden_state
+        
+        # perform mean pooling 
+        mask_exp = input_mask.unsqueeze(-1).expand(last_hidden_states.size()).long()
+        sum_masked_output = torch.sum(last_hidden_states * mask_exp, 1)
+        sum_mask = torch.clamp(mask_exp.sum(1), min=1e-4)
+        output = sum_masked_output / sum_mask     
+        output = nn.functional.normalize(output, 1) # normlaize result
+        
+        return output
         
     def forward(self, batched_hr_token_ids, batched_hr_mask, batched_hr_token_type_ids,
                 batched_tail_token_ids, batched_tail_mask, batched_tail_token_type_ids, **kwargs): 
